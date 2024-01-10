8f8113ba402331147640c9ee5314d731db298d67
arck <prueba.arck@gmail.com>
2024-01-10 21:09:19 +0100

    modificaciones

--- angular/clientesApp/src/app/clientes/cliente.service.ts
+++ angular/clientesApp/src/app/clientes/cliente.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { CLIENTES } from './clientes.json';
 import { Cliente } from './cliente';
 import { of, Observable } from 'rxjs';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs';
 
 @Injectable()
@@ -10,11 +10,17 @@ export class ClienteService {
 
   private url: string = "http://localhost:8080/api/clientes"; 
 
+  private httpHeader = new HttpHeaders({'Content-Type': 'application/json'})
   constructor(private http: HttpClient){}
 
   getClientes(): Observable<Cliente[]>{
     return this.http.get(this.url).pipe(
       map(response => response as Cliente[])
     );
+  }
+
+  create(cliente: Cliente): Observable<Cliente>{
+    console.log("cliente service: "+cliente)
+    return this.http.post<Cliente>(this.url,cliente,{headers: this.httpHeader});
   }
 }
--- angular/clientesApp/src/app/clientes/form.component.ts
+++ angular/clientesApp/src/app/clientes/form.component.ts
@@ -1,23 +1,25 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Cliente } from './cliente';
+import { ClienteService } from './cliente.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html'
 })
-export class FormComponent implements OnInit{
+export class FormComponent{
 
   public cliente: Cliente = new Cliente()
   public titulo:string = "Crear Cliente"
 
-  contructor(){}
-
-  ngOnInit(){}
+  constructor(private clienteService: ClienteService, 
+    private router: Router){
+    }
 
   public create():void{
-    console.log("Clicked");
-    console.log(this.cliente);
+    console.log(this.cliente)
+    this.clienteService.create(this.cliente).subscribe(
+        response => this.router.navigate(['/clientes']))    
   }
-
 }
